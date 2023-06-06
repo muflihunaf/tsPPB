ddd60207561a0be669e6e17e47ca44ef2977ae07
FridhaMegantaraPutra354 <taramegan87@gmail.com>
2023-06-06 17:03:29 +0700

    'megantara'

---
+++ app/src/main/java/exportkit/figma/android_small___3_activity.java
@@ -0,0 +1,76 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_small___3
+	 *	@date 		Tuesday 06th of June 2023 09:36:03 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class android_small___3_activity extends Activity {
+
+	
+	private View _bg__android_small___3_ek2;
+	private ImageView rectangle_1;
+	private TextView cari_wisata_tujuan;
+	private ImageView pngwing_1;
+	private ImageView rectangle_4;
+	private TextView sight;
+	private TextView trip_plan;
+	private TextView review;
+	private ImageView user_1;
+	private ImageView _20230410_100000_2;
+	private ImageView _1681017729812_2;
+	private TextView bukit_arosbaya;
+	private ImageView _1681017683474_2;
+	private TextView bukit_jedih;
+	private ImageView airplane_paper_plane_clip_art_paper_plane_51cc22441b47d3ce0ebd2b87d50d3141_1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_small___3);
+
+		
+		_bg__android_small___3_ek2 = (View) findViewById(R.id._bg__android_small___3_ek2);
+		rectangle_1 = (ImageView) findViewById(R.id.rectangle_1);
+		cari_wisata_tujuan = (TextView) findViewById(R.id.cari_wisata_tujuan);
+		pngwing_1 = (ImageView) findViewById(R.id.pngwing_1);
+		rectangle_4 = (ImageView) findViewById(R.id.rectangle_4);
+		sight = (TextView) findViewById(R.id.sight);
+		trip_plan = (TextView) findViewById(R.id.trip_plan);
+		review = (TextView) findViewById(R.id.review);
+		user_1 = (ImageView) findViewById(R.id.user_1);
+		_20230410_100000_2 = (ImageView) findViewById(R.id._20230410_100000_2);
+		_1681017729812_2 = (ImageView) findViewById(R.id._1681017729812_2);
+		bukit_arosbaya = (TextView) findViewById(R.id.bukit_arosbaya);
+		_1681017683474_2 = (ImageView) findViewById(R.id._1681017683474_2);
+		bukit_jedih = (TextView) findViewById(R.id.bukit_jedih);
+		airplane_paper_plane_clip_art_paper_plane_51cc22441b47d3ce0ebd2b87d50d3141_1 = (ImageView) findViewById(R.id.airplane_paper_plane_clip_art_paper_plane_51cc22441b47d3ce0ebd2b87d50d3141_1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
